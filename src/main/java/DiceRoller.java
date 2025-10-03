2361d48829f2cf3090d0f887cf453d4524524fcb
bara-codes <198811843+bara-codes@users.noreply.github.com>
2025-10-03 14:04:43 -0400

    done

--- src/main/java/DiceRoller.java
+++ src/main/java/DiceRoller.java
@@ -2,6 +2,15 @@ import java.util.Random;
 
 public class DiceRoller {
     public static void main(String[] args) {
+        Random rand1 = new Random();
+        Random rand2 = new Random();
+        Random rand3 = new Random();
+        Random rand4 = new Random(5);
+
+        System.out.println("Unseeded: " + rand1.nextInt(5));
+        System.out.println("Unseeded: " + rand2.nextInt(5));
+        System.out.println("Unseeded: " + rand3.nextInt(5));
+        System.out.println("Seeded: " + rand4.nextInt(5));
         // TODO: Create Random objects for dice rolling
         // 1. Three Random objects without seeds (unseeded dice)
         // 2. One Random object with any seed value (seeded die)
--- src/main/java/PhoneNumberGenerator.java
+++ src/main/java/PhoneNumberGenerator.java
@@ -1,19 +1,26 @@
 import java.util.Random;
+import java.text.DecimalFormat;
 
 public class PhoneNumberGenerator {
     public static void main(String[] args) {
+        Random rand1 = new Random();
+        DecimalFormat df = new DecimalFormat("000");
+
         // TODO: Create Random object for phone number generation
         
         // TODO: Generate first three digits (XXX)
         // Constraint: Cannot contain 8 or 9 (digits 0-7 only)
-        
+        int dig1 = rand1.nextInt(8);
+        int dig2 = rand1.nextInt(8);
+        int dig3 = rand1.nextInt(8);
         // TODO: Generate second three digits (XXX) 
         // Constraint: Must be <= 742 (000-742)
-        
+        int dig456 = rand1.nextInt(743);
         // TODO: Generate third three digits (XXX)
         // No constraints (000-999)
-        
+        int dig789 = rand1.nextInt(1000);
         // TODO: Format and display phone number as XXX-XXX-XXX
         // Include the dashes in the output
+        System.out.printf("%d%d%d-%s-%s", dig1, dig2, dig3, df.format(dig456), df.format(dig789));
     }
 }
